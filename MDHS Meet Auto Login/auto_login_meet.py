279aeab1dc0e30003f2275d23410b427b8fe611d
iceysheep157 <58882598+iceysheep157@users.noreply.github.com>
2021-05-31 08:38:37 +0800

    minor change

--- MDHS Meet Auto Login/auto_login_meet.py
+++ MDHS Meet Auto Login/auto_login_meet.py
@@ -15,10 +15,9 @@ for i in range(4):
 opt = Options()
 opt.add_argument("--disable-infobars")
 opt.add_argument("start-maximized")
-opt.add_argument("--disable-extensions")
 
 opt.add_experimental_option("prefs", { \
-    "profile.default_content_setting_values.media_stream_mic": 1,
+    "profile.default_content_setting_values.media_stream_mic": 2,
     "profile.default_content_setting_values.media_stream_camera": 1,
     "profile.default_content_setting_values.geolocation": 1,
     "profile.default_content_setting_values.notifications": 1
